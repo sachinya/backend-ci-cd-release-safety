6eb5c8ffd540a3be825eeae09c3cd019cd348fa9
Sachin Yadav <ysachin704@gmail.com>
2026-01-06 15:56:24 +0530

    Initial backend service for ci/cd project

---
+++ src/main/java/com/example/backend_ci_cd_release_safety/BackendCiCdReleaseSafetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_ci_cd_release_safety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCiCdReleaseSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCiCdReleaseSafetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_ci_cd_release_safety/BackendCiCdReleaseSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_ci_cd_release_safety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCiCdReleaseSafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
